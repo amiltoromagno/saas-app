4ae87c09b263e732587a49a76f43ba11a6908ec9
Amilto Romagno <amiltoromagno@outlook.com>
2024-10-25 09:08:22 -0300

    fix: change base url to fix images in production

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'production' ? '/saas-app/' : '/',
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/saas-app/'
 })
