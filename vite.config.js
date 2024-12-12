64ca4bc5059e90d9c286ef59a16b6a547cd614ce
Amilto Romagno <amiltoromagno@outlook.com>
2024-12-12 19:29:06 -0300

    fix: change base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://saas-nyqbzk14x-amiltos-projects.vercel.app/'
+  base: 'https://amilto-saas.vercel.app/'
 })
